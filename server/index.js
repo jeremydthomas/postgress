b681d1e16592cb2d44d51969429f9f88826c1cfe
jeremydthomas <tommyhilfiger12@hotmail.com>
2023-01-28 12:17:21 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+app.use(cors());
+app.use(express.json());
+
+// ROUTES //
+
+// create a todo
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
+app.listen(5000, () => {
+	console.log("Server started on port 5000");
+});
